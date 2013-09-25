9edf4472b4ef0b7bdc718b182549452bfe37aafa
igor.zc <igor.zc@5f9a0195-93d7-7a6b-d9ba-c9014156c6cf>
2013-09-25 07:45:33 +0000

    added section ", updated {1}" for temperature producer

--- Stable/Sources/ModiinNewsAggregator/ModiinNewsAggregator/PogodaModiinProducer.cs
+++ Stable/Sources/ModiinNewsAggregator/ModiinNewsAggregator/PogodaModiinProducer.cs
@@ -16,9 +16,13 @@ namespace ModiinNewsAggregator
             htmlDoc.Load(new StringReader(htmlContent));
 
             HtmlNode currentTemperature = htmlDoc.DocumentNode.SelectSingleNode(@"/html[1]/body[1]/div[1]/div[4]/div[2]/div[1]/strong[1]");
-            string innerText = currentTemperature.InnerText;
-            string[] contentStrings = innerText.Split('&');
-            return String.Format("#Modiin - now {0}\u00B0C. Source: http://pogoda.co.il/israel/modiin", contentStrings[0]);
+            string currentTemperatureText = currentTemperature.InnerText;
+            string[] contentStrings = currentTemperatureText.Split('&');
+            
+            HtmlNode dataUpdated = htmlDoc.DocumentNode.SelectSingleNode(@"/html/body/div/div[4]/div[2]/div[1]/p[2]/b[1]");
+            string dataUpdatedText = dataUpdated.InnerText;
+
+            return String.Format("#Modiin - now {0}\u00B0C. Source: http://pogoda.co.il/israel/modiin, updated {1}", contentStrings[0], dataUpdatedText);
         }
         #endregion
         private string GetPogodaModiinPage()
