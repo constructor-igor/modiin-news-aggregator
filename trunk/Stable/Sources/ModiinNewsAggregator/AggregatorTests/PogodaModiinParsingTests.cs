d32f044fec77f93fd0610c31354f14b1b0a732e7
igor.zc <igor.zc@5f9a0195-93d7-7a6b-d9ba-c9014156c6cf>
2013-09-25 07:41:24 +0000

    added test ParsePogodaModiin_Updated_1150

---
+++ trunk/Stable/Sources/ModiinNewsAggregator/AggregatorTests/PogodaModiinParsingTests.cs
@@ -0,0 +1,53 @@
+﻿using System.IO;
+using HtmlAgilityPack;
+using NUnit.Framework;
+
+namespace AggregatorTests
+{
+    [TestFixture]
+    public class PogodaModiinParsingTests
+    {
+        [Test]
+        public void ParsePogodaModiin_Temperature_28()
+        {
+            //            const string xPath = @"/html/body/table/tbody/tr[547]/td[2]";
+            const string xPath = @"/html[1]/body[1]/div[1]/div[4]/div[2]/div[1]/strong[1]";
+
+            string htmlContent = File.ReadAllText(@"..\..\Data\PogodaModdin.html");
+
+            var htmlDoc = new HtmlDocument { OptionFixNestedTags = true };
+            htmlDoc.Load(new StringReader(htmlContent));
+
+            HtmlNode currentTemperature = htmlDoc.DocumentNode.SelectSingleNode(xPath);
+            Assert.AreEqual("+28&deg;", currentTemperature.InnerText);
+
+            //            HtmlNode nodeBody = htmlDoc.DocumentNode.SelectSingleNode(@"/html/body/div/div[4]/div[2]/div[1]/strong");
+            //            foreach (HtmlNode node in nodeBody.ChildNodes)
+            //            {
+            //                Trace.WriteLine(String.Format("**** node name: {0}", node.Name));
+            //                Trace.WriteLine(String.Format("**** node Inner Text: {0}, Html: {1}, XPath: {2}", node.InnerText, node.InnerHtml, node.XPath));
+            //            }
+            //            HtmlNode node = doc.DocumentNode.SelectSingleNode(xPath);
+            //            Assert.AreEqual("28", node.OuterHtml);
+        }
+
+        [Test]
+        public void ParsePogodaModiin_Updated_1150()
+        {
+            const string xPath = @"/html/body/div/div[4]/div[2]/div[1]/p[2]/b[1]";
+
+            string htmlContent = File.ReadAllText(@"..\..\Data\PogodaModdin.html");
+            var htmlDoc = new HtmlDocument { OptionFixNestedTags = true };
+            htmlDoc.Load(new StringReader(htmlContent));
+
+//            HtmlNode nodeBody = htmlDoc.DocumentNode.SelectSingleNode(@"/html/body/div/div[4]/div[2]/div[1]/p[2]/b[1]");
+//            foreach (HtmlNode node in nodeBody.ChildNodes)
+//            {
+//                System.Diagnostics.Trace.WriteLine(String.Format("**** node name: {0}", node.Name));
+//                System.Diagnostics.Trace.WriteLine(String.Format("**** node Inner Text: {0}, Html: {1}, XPath: {2}", node.InnerText, node.InnerHtml, node.XPath));
+//            }
+            HtmlNode updateNode = htmlDoc.DocumentNode.SelectSingleNode(xPath);
+            Assert.AreEqual("11:50", updateNode.InnerText);
+        }
+    }
+}
