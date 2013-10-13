35ac109d48d4bc137164d9eca41c351564ef0d8e
igor.zc <igor.zc@5f9a0195-93d7-7a6b-d9ba-c9014156c6cf>
2013-10-13 20:24:30 +0000

    updated logs

--- trunk/Stable/Sources/ModiinNewsAggregator/ModiinNewsAggregator/Producers/LogDecoratorProducer.cs
+++ trunk/Stable/Sources/ModiinNewsAggregator/ModiinNewsAggregator/Producers/LogDecoratorProducer.cs
@@ -8,16 +8,19 @@ namespace ModiinNewsAggregator.Producers
         private static readonly Logger m_logger = LogManager.GetCurrentClassLogger();
 
         private readonly IProducer actualProducer;
+        private readonly bool includeEmptyMessage;
 
-        public LogDecoratorProducer(IProducer actualProducer)
+        public LogDecoratorProducer(IProducer actualProducer, bool includeEmptyMessage)
         {
             this.actualProducer = actualProducer;
+            this.includeEmptyMessage = includeEmptyMessage;
         }
         #region IProducer
         public IMessage GetMessage()
         {
             IMessage actualContent = actualProducer.GetMessage();
-            m_logger.Trace("IProducer.GetMessage(): {0}", actualContent);
+            if (!actualContent.Empty || includeEmptyMessage)
+                m_logger.Trace("GetMessage(): {0}", actualContent);
             return actualContent;
         }
         #endregion
