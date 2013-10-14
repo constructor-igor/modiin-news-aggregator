edc4696db9dc3d16d697c5652ed87fef6a34374e
igor.zc <igor.zc@5f9a0195-93d7-7a6b-d9ba-c9014156c6cf>
2013-10-14 13:53:48 +0000

    minor files redesign

---
+++ Stable/Sources/ModiinNewsAggregator/AggregatorTests/Feasibility/WatiNTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using HtmlAgilityPack;
+using NUnit.Framework;
+using WatiN.Core;
+
+namespace AggregatorTests.Feasibility
+{
+    [TestFixture]
+    public class WatiNTests
+    {
+        [Test]
+        [Explicit]
+        public void LoadHtmlWatiN_PogodaModiin_Grabbing()
+        {
+            using (var browser = new IE("http://pogoda.co.il/israel/modiin"))
+            {
+                Settings.WaitForCompleteTimeOut = 999999999; /*увеличиваем тайм-аут на всякий случай*/
+                var doc = new HtmlDocument(); /* инсталляция объекта парсера HTML Agility Pack*/
+                doc.LoadHtml(browser.Body.OuterHtml); /*помещаем в парсер полученный html с страницы yvison.kz*/
+                Console.WriteLine(doc.ToString());
+            }
+            Assert.Pass();
+        }
+    }
+}
--- Stable/Sources/ModiinNewsAggregator/AggregatorTests/Feasibility/WebClientTests.cs
+++ Stable/Sources/ModiinNewsAggregator/AggregatorTests/Feasibility/WebClientTests.cs
@@ -1,29 +1,13 @@
 ﻿using System;
 using System.IO;
 using System.Net;
-using HtmlAgilityPack;
 using NUnit.Framework;
-using WatiN.Core;
 
 namespace AggregatorTests.Feasibility
 {
     [TestFixture]
-    public class FeasibilityTests
+    public class WebClientTests
     {
-        [Test]
-        [Explicit]
-        public void LoadHtmlWatiN_PogodaModiin_Grabbing()
-        {
-            using (var browser = new IE("http://pogoda.co.il/israel/modiin"))
-            {
-                Settings.WaitForCompleteTimeOut = 999999999; /*увеличиваем тайм-аут на всякий случай*/
-                var doc = new HtmlDocument(); /* инсталляция объекта парсера HTML Agility Pack*/
-                doc.LoadHtml(browser.Body.OuterHtml); /*помещаем в парсер полученный html с страницы yvison.kz*/
-                Console.WriteLine(doc.ToString());
-            }
-            Assert.Pass();
-        }
-
         [Test]
         [Explicit]
         // http://msdn.microsoft.com/en-us/library/system.net.webclient.aspx
