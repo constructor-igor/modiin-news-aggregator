e4894fd9d40503c884ec013bc5934613d7bb9e34
igor.zc <igor.zc@5f9a0195-93d7-7a6b-d9ba-c9014156c6cf>
2013-10-14 14:29:24 +0000

    redesign: implemented  IAggregatorExecuter

--- trunk/Stable/Sources/ModiinNewsAggregator/ModiinNewsAggregator/Program.cs
+++ trunk/Stable/Sources/ModiinNewsAggregator/ModiinNewsAggregator/Program.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
+using ModiinNewsAggregator.Executers;
 using ModiinNewsAggregator.Interfaces;
 using ModiinNewsAggregator.Producers;
 using ModiinNewsAggregator.Senders;
@@ -46,14 +47,12 @@ namespace ModiinNewsAggregator
                         Console.WriteLine(instagramMedia.GetMessage());
                         break;
                     case "start":
-                        Timer pogodaTimer = StartModiinPogodaTimer();
-                        Timer liveJournlTimer = StartLiveJournalMonitoringTimer();
-
-                        GC.KeepAlive(pogodaTimer);
-                        GC.KeepAlive(liveJournlTimer);
+                        var timerExecuter = new TimerExecuter();
+                        timerExecuter.Start();
                         break;
                     case "start2":
-                        Start2();
+                        IAggregatorExecuter taskExecuter = new TaskExecuter();
+                        taskExecuter.Start();
                         break;
                     default:
                         Console.WriteLine("unknown command: '{0}'", command);
@@ -64,100 +63,6 @@ namespace ModiinNewsAggregator
             m_logger.Info("ModiinNewsAggregator finished");
         }
 
-        private static Timer StartModiinPogodaTimer()
-        {
-            const int SEC1 = 1000;
-            const int MIN1 = 60*SEC1;
-            const int HOUR1 = 60*MIN1;
-            const int PERIOD = HOUR1;
-            var timer = new Timer(state =>
-            {
-                IMessage message = new MessageContainer();                
-                try
-                {
-                    IProducer takeCurrentWeather = new LogDecoratorProducer(new UpdatesProducer(new PogodaModiinProducer()), includeEmptyMessage: false);
-                    ISender masterSender = new EmptyFilterSender(new TwitterSender());
-                    message = takeCurrentWeather.GetMessage();
-                    masterSender.Send(message);
-
-                    IProducer instagram = new LogDecoratorProducer(new UpdatesProducer(new InstagramProducer()), includeEmptyMessage: false);
-                    masterSender.Send(instagram.GetMessage());
-                }
-                catch (TwitterErrorException e)
-                {
-                    m_logger.WarnException(String.Format("Twit '{0}' not sent, because {1}", message.Text, e.Status.Content), e);
-                }
-            }, null, 0, PERIOD);
-            return timer;
-        }
-        private static Timer StartLiveJournalMonitoringTimer()
-        {
-            const int SEC1 = 1000;
-            const int MIN1 = 60 * SEC1;
-            const int HOUR1 = 60 * MIN1;
-            const int PERIOD = MIN1;
-            IProducer takeLiveJournalUpdate = new LogDecoratorProducer(new UpdatesProducer(new LiveJournalProducer()), includeEmptyMessage: false);
-            var timer = new Timer(state =>
-            {
-                IMessage message = new MessageContainer();
-                try
-                {
-                    var producer = (IProducer) state;
-                    ISender sender = new EmptyFilterSender(new LogDecoratorSender(new TwitterSender()));
-                    message = producer.GetMessage();
-                    sender.Send(message);
-                }
-                catch (TwitterErrorException e)
-                {
-                    m_logger.WarnException(String.Format("Twit '{0}' not sent, because {1}", message.Text, e.Status.Content), e);
-                }
-            }, takeLiveJournalUpdate, 0, PERIOD);
-            return timer;
-        }
-
-        public static void Start2()
-        {
-            var queue = new ConcurrentQueue<IMessage>();
-            Task wheatherProducerTask = Task.Factory.StartNew(() =>
-            {
-                IProducer takeCurrentWeather = new LogDecoratorProducer(new UpdatesProducer(new PogodaModiinProducer()), includeEmptyMessage: false);
-                while (true)
-                {                    
-                    queue.Enqueue(takeCurrentWeather.GetMessage());
-                    Thread.Sleep(new TimeSpan(hours: 1, minutes: 0, seconds: 0));
-                }
-            });
-            Task instagramProducerTask = Task.Factory.StartNew(() =>
-            {
-                IProducer instagramModiin = new LogDecoratorProducer(new UpdatesProducer(new InstagramProducer()), includeEmptyMessage: false);
-                while (true)
-                {                    
-                    queue.Enqueue(instagramModiin.GetMessage());
-                    Thread.Sleep(new TimeSpan(hours: 1, minutes: 0, seconds: 0));
-                }
-            });
-            Task ModiinLjTask = Task.Factory.StartNew(() =>
-            {
-                IProducer takeLiveJournalUpdate = new LogDecoratorProducer(new UpdatesProducer(new LiveJournalProducer()), includeEmptyMessage: false);
-                while (true)
-                {                    
-                    queue.Enqueue(takeLiveJournalUpdate.GetMessage());
-                    Thread.Sleep(new TimeSpan(hours: 0, minutes: 1, seconds: 0));
-                }
-            });
-            Task twitterSenderTask = Task.Factory.StartNew(() =>
-            {
-                ISender sender = new EmptyFilterSender(new LogDecoratorSender(new TwitterSender()));
-                while (true)
-                {                    
-                    IMessage message;
-                    if (queue.TryDequeue(out message))
-                    {                        
-                        sender.Send(message);
-                    }
-                }
-            });
-        }
         public static string Version
         {
             get
@@ -168,5 +73,4 @@ namespace ModiinNewsAggregator
             }
         }
     }
-
 }
