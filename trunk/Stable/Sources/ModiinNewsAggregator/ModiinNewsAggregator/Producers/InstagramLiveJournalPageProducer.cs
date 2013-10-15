2e7fa9cf3a5c06ce9c7a6cfef9531629e1653624
igor.zc <igor.zc@5f9a0195-93d7-7a6b-d9ba-c9014156c6cf>
2013-10-15 21:45:26 +0000

    implemented command instagramToLJ

---
+++ trunk/Stable/Sources/ModiinNewsAggregator/ModiinNewsAggregator/Producers/InstagramLiveJournalPageProducer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+using ModiinNewsAggregator.Interfaces;
+
+namespace ModiinNewsAggregator.Producers
+{
+    public class InstagramLiveJournalPageProducer : IProducer
+    {
+        private readonly IProducer instagProducer;
+        public InstagramLiveJournalPageProducer(IProducer instagProducer)
+        {
+            this.instagProducer = instagProducer;
+        }
+        #region IProducer
+        public IMessage GetMessage()
+        {
+            IMessage message = instagProducer.GetMessage();
+            var instagramMediaDataMessage = message as InstagramMediaDataMessage;
+            if (instagramMediaDataMessage == null)
+                return new MessageContainer();
+
+            var actualLiveJournalPostAsHtml = new StringBuilder();
+            actualLiveJournalPostAsHtml.AppendLine();
+            actualLiveJournalPostAsHtml.AppendLine("<lj-cut></lj-cut>");
+
+            foreach (InstagramMedia media in instagramMediaDataMessage.MediaList)
+            {
+                actualLiveJournalPostAsHtml.AppendLine();
+                actualLiveJournalPostAsHtml.AppendFormat("{0} by {1}", media.Caption, media.Author);
+                if (!String.IsNullOrEmpty(media.Latitude))
+                {
+                    actualLiveJournalPostAsHtml
+                        .AppendLine(String.Format(@" <a href=""https://www.google.com/maps?q={0},{1}&amp;num=1&amp;t=m&amp;z=18"" target=""_blank"">on Google Map</a>", media.Latitude, media.Longitude));
+                }
+                actualLiveJournalPostAsHtml.AppendLine(String
+                        .Format(@"<img alt="""" border=""0"" src=""{0}"" title=""{1}"" />", media.FileUrl, media.Caption)
+                        .Replace(@"\/", @"/"));
+            }
+
+            return new MessageContainer(actualLiveJournalPostAsHtml.ToString());
+        }
+        #endregion
+    }
+}
