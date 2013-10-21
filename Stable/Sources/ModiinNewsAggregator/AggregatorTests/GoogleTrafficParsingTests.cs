f7c3b0fb3d029ff55605e8b32aa08cc3ab802e80
igor.zc <igor.zc@5f9a0195-93d7-7a6b-d9ba-c9014156c6cf>
2013-10-21 11:20:58 +0000

    update "google traffic"

--- Stable/Sources/ModiinNewsAggregator/AggregatorTests/GoogleTrafficParsingTests.cs
+++ Stable/Sources/ModiinNewsAggregator/AggregatorTests/GoogleTrafficParsingTests.cs
@@ -18,7 +18,7 @@ namespace AggregatorTests
             //            const string xPath = @"/html/body/table/tbody/tr[547]/td[2]";
             //const string xPath = @"/html[1]/body[1]/div[1]/div[4]/div[2]/div[1]/strong[1]";
             //const string xPath = @"/html[1]/body/div/div/div[3]/div[6]/div/div[2]/div[3]/div/div/div/div/ol/li/div";
-            const string xPath = @"/html[1]/body[1]/div[4]/div[6]/div[1]/div[2]/div[3]/div[1]/div[1]/div[1]/div[1]/ol[1]/li[1]/div[1]/div[2]";
+            const string xPath = @"/html[1]/body[1]/div[4]/div[6]/div[1]/div[2]/div[3]/div[1]/div[1]/div[1]/div[1]/ol[1]/li[1]/div[1]/div[2]/span[1]";
             string htmlContent = File.ReadAllText(@"..\..\Data\GoogleTrafficRehovotModiin.html");
             //
             var htmlDoc = new HtmlDocument { OptionFixNestedTags = true };
@@ -31,11 +31,12 @@ namespace AggregatorTests
                 if (childNode.InnerHtml.Contains("In current"))
                 {
                     Trace.WriteLine("XPath: ", childNode.XPath);
+                    Trace.WriteLine("Inner Text: ", childNode.InnerText);
                 }
             }
 
             Assert.IsNotNull(inCurrentTrafficNode);
-            Assert.AreEqual("   In current traffic: 28 mins   ", inCurrentTrafficNode.InnerText);
+            Assert.AreEqual(" In current traffic: 28 mins ", inCurrentTrafficNode.InnerText);
         }
         [Test]
         public void Parse_StandardTraffic25minutes()
@@ -63,6 +64,34 @@ namespace AggregatorTests
             Assert.IsNotNull(standardTrafficNode);
             Assert.AreEqual("25 mins", standardTrafficNode.InnerText);
         }
+    
+        [Test]
+        public void Parse_RouteNameRoute431()
+        {
+            //            const string xPath = @"/html/body/table/tbody/tr[547]/td[2]";
+            //const string xPath = @"/html[1]/body[1]/div[1]/div[4]/div[2]/div[1]/strong[1]";
+            //const string xPath = @"/html[1]/body/div/div/div[3]/div[6]/div/div[2]/div[3]/div/div/div/div/ol/li/div";
+            //const string xPath = @"/html[1]/body[1]/div[4]/div[6]/div[1]/div[2]/div[3]/div[1]/div[1]/div[1]/div[1]/ol[1]/li[1]/div[1]/div[2]";
+            const string xPath = @"/html[1]/body[1]/div[4]/div[6]/div[1]/div[2]/div[3]/div[1]/div[1]/div[1]/div[1]/ol[1]/li[1]/div[1]/div[3]";
+            string htmlContent = File.ReadAllText(@"..\..\Data\GoogleTrafficRehovotModiin.html");
+            //
+            var htmlDoc = new HtmlDocument { OptionFixNestedTags = true };
+            htmlDoc.Load(new StringReader(htmlContent));
+
+            HtmlNode standardTrafficNode = htmlDoc.DocumentNode.SelectSingleNode(xPath);
+
+            foreach (HtmlNode childNode in standardTrafficNode.ChildNodes)
+            {
+                if (childNode.InnerHtml.Contains("Route 431"))
+                {
+                    Trace.WriteLine("XPath: ", childNode.XPath);
+                    Trace.WriteLine("InnerText: ", childNode.InnerText);
+                }
+            }
+
+            Assert.IsNotNull(standardTrafficNode);
+            Assert.AreEqual("Route 431", standardTrafficNode.InnerText);
+        }
     }
 
     [TestFixture]
@@ -78,10 +107,9 @@ namespace AggregatorTests
                
                 var producer = new GoogleTrafficProducer(mock.Object);
                 var message = producer.GetMessage() as GoogleTrafficMessage;
-                Assert.AreEqual("In current traffic: 28 mins (Standard: 25 mins), In current traffic: 38 mins (Standard: 32 mins)", message.Text);
+                const string expectedText = "Route 431: 28 mins (25 mins), Route 412 and Route 431: 38 mins (32 mins)";
+                Assert.AreEqual(expectedText, message.Text);
                 Assert.AreEqual(2, message.SuggestedTraffic.Count);
-                Assert.AreEqual("   In current traffic: 28 mins   ", message.SuggestedTraffic[0].InCurrentTraffic);
-                Assert.AreEqual("   In current traffic: 38 mins   ", message.SuggestedTraffic[1].InCurrentTraffic);
             }
         }
     }
---
+++ Stable/Sources/ModiinNewsAggregator/ModiinNewsAggregator/Executers/TrafficExecuter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+using ModiinNewsAggregator.Interfaces;
+using ModiinNewsAggregator.Producers;
+using ModiinNewsAggregator.Senders;
+
+namespace ModiinNewsAggregator.Executers
+{
+    public class TrafficExecuter : IAggregatorExecuter
+    {
+        public void Start()
+        {
+            var minutes5 = new TimeSpan(hours: 0, minutes: 5, seconds: 0);
+            const string uri = @"https://www.google.com/maps?sll=31.89527449987709%2C34.98711830006862&sspn=0.11295803168870334%2C0.27472621411297554&saddr=rehovot&t=m&output=classic&daddr=Modi%27in-Maccabim-Re%27ut%2C+Israel&dirflg=d";
+
+            var queue = new ConcurrentQueue<IMessage>();
+            Task weatherProducerTask = Task.Factory.StartNew(() =>
+            {
+                IProducer takeCurrentWeather = new LogDecoratorProducer(new UpdatesProducer(new GoogleTrafficProducer(new UriStreamCreator(uri))), includeEmptyMessage: false);
+                while (true)
+                {
+                    queue.Enqueue(takeCurrentWeather.GetMessage());
+                    Thread.Sleep(minutes5);
+                }
+            });
+
+            Task twitterSenderTask = Task.Factory.StartNew(() =>
+            {
+                ISender sender = new EmptyFilterSender(new LogDecoratorSender(new TwitterSender()));
+                while (true)
+                {
+                    IMessage message;
+                    if (queue.TryDequeue(out message))
+                    {
+                        sender.Send(message);
+                    }
+                }
+            });
+        }
+    }
+}
