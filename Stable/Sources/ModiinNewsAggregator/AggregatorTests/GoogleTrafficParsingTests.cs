26518a7b088acd9f30f82a5fc7f48bee7694ae73
igor.zc <igor.zc@5f9a0195-93d7-7a6b-d9ba-c9014156c6cf>
2013-10-21 10:43:37 +0000

    added Standard & Current traffic in GoogleTrafficMessage

--- Stable/Sources/ModiinNewsAggregator/AggregatorTests/GoogleTrafficParsingTests.cs
+++ Stable/Sources/ModiinNewsAggregator/AggregatorTests/GoogleTrafficParsingTests.cs
@@ -36,7 +36,32 @@ namespace AggregatorTests
 
             Assert.IsNotNull(inCurrentTrafficNode);
             Assert.AreEqual("   In current traffic: 28 mins   ", inCurrentTrafficNode.InnerText);
+        }
+        [Test]
+        public void Parse_StandardTraffic25minutes()
+        {
+            //            const string xPath = @"/html/body/table/tbody/tr[547]/td[2]";
+            //const string xPath = @"/html[1]/body[1]/div[1]/div[4]/div[2]/div[1]/strong[1]";
+            //const string xPath = @"/html[1]/body/div/div/div[3]/div[6]/div/div[2]/div[3]/div/div/div/div/ol/li/div";
+            //const string xPath = @"/html[1]/body[1]/div[4]/div[6]/div[1]/div[2]/div[3]/div[1]/div[1]/div[1]/div[1]/ol[1]/li[1]/div[1]/div[2]";
+            const string xPath = @"/html[1]/body[1]/div[4]/div[6]/div[1]/div[2]/div[3]/div[1]/div[1]/div[1]/div[1]/ol[1]/li[1]/div[1]/div[1]/span[2]";
+            string htmlContent = File.ReadAllText(@"..\..\Data\GoogleTrafficRehovotModiin.html");
+            //
+            var htmlDoc = new HtmlDocument { OptionFixNestedTags = true };
+            htmlDoc.Load(new StringReader(htmlContent));
 
+            HtmlNode standardTrafficNode = htmlDoc.DocumentNode.SelectSingleNode(xPath);
+
+            foreach (HtmlNode childNode in standardTrafficNode.ChildNodes)
+            {
+                if (childNode.InnerHtml.Contains("25 mins"))
+                {
+                    Trace.WriteLine("XPath: ", childNode.XPath);
+                }
+            }
+
+            Assert.IsNotNull(standardTrafficNode);
+            Assert.AreEqual("25 mins", standardTrafficNode.InnerText);
         }
     }
 
@@ -53,8 +78,10 @@ namespace AggregatorTests
                
                 var producer = new GoogleTrafficProducer(mock.Object);
                 var message = producer.GetMessage() as GoogleTrafficMessage;
-                Assert.AreEqual("   In current traffic: 28 mins   ", message.Text);
-                //Assert.AreEqual(2, message.SuggestedTraffic.Count);
+                Assert.AreEqual("In current traffic: 28 mins (Standard: 25 mins), In current traffic: 38 mins (Standard: 32 mins)", message.Text);
+                Assert.AreEqual(2, message.SuggestedTraffic.Count);
+                Assert.AreEqual("   In current traffic: 28 mins   ", message.SuggestedTraffic[0].InCurrentTraffic);
+                Assert.AreEqual("   In current traffic: 38 mins   ", message.SuggestedTraffic[1].InCurrentTraffic);
             }
         }
     }
