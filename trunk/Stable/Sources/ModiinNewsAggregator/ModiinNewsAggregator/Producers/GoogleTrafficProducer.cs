e779298074f65ed1f58e74c32658566322219ac7
igor.zc <igor.zc@5f9a0195-93d7-7a6b-d9ba-c9014156c6cf>
2013-10-21 10:43:37 +0000

    added Standard & Current traffic in GoogleTrafficMessage

--- trunk/Stable/Sources/ModiinNewsAggregator/ModiinNewsAggregator/Producers/GoogleTrafficProducer.cs
+++ trunk/Stable/Sources/ModiinNewsAggregator/ModiinNewsAggregator/Producers/GoogleTrafficProducer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
 using HtmlAgilityPack;
 using ModiinNewsAggregator.Interfaces;
 
@@ -16,17 +17,31 @@ namespace ModiinNewsAggregator.Producers
         #region IProducer
         public IMessage GetMessage()
         {
-            const string xPath = @"/html[1]/body[1]/div[4]/div[6]/div[1]/div[2]/div[3]/div[1]/div[1]/div[1]/div[1]/ol[1]/li[1]/div[1]/div[2]";
+            //const string xPath = @"/html[1]/body[1]/div[4]/div[6]/div[1]/div[2]/div[3]/div[1]/div[1]/div[1]/div[1]/ol[1]/li[1]/div[1]/div[2]";
+            const string routePath = @"/html[1]/body[1]/div[4]/div[6]/div[1]/div[2]/div[3]/div[1]/div[1]/div[1]/div[1]/ol[1]/li[{0}]";
+//            const string currentTrafficPath = @"/html[1]/body[1]/div[4]/div[6]/div[1]/div[2]/div[3]/div[1]/div[1]/div[1]/div[1]/ol[1]/li[{0}]/div[1]/div[2]";
+//            const string standardTrafficPath = @"/html[1]/body[1]/div[4]/div[6]/div[1]/div[2]/div[3]/div[1]/div[1]/div[1]/div[1]/ol[1]/li[{0}]/div[1]/div[1]/span[2]";
 
             using (Stream stream = streamCreator.CreateStream())
             {
                 var htmlDoc = new HtmlDocument {OptionFixNestedTags = true};
                 htmlDoc.Load(stream);
 
-                HtmlNode inCurrentTrafficNode = htmlDoc.DocumentNode.SelectSingleNode(xPath);
-
                 var message = new GoogleTrafficMessage();
-                message.SuggestedTraffic.Add(new GoogleTrafficRoute { InCurrentTraffic = inCurrentTrafficNode.InnerText});
+                HtmlNode routeNode = null;
+                int routeIndex = 1;
+                while ((routeNode = htmlDoc.DocumentNode.SelectSingleNode(String.Format(routePath, routeIndex++))) != null)
+                {
+                    HtmlNodeCollection standardTrafficNode = routeNode.SelectNodes("div[1]/div[1]/span[2]");
+                    HtmlNodeCollection currentTrafficNode = routeNode.SelectNodes("div[1]/div[2]");
+
+                    var googleTrafficRoute = new GoogleTrafficRoute
+                    {
+                        StandardTraffic = standardTrafficNode[0].InnerText,
+                        InCurrentTraffic = currentTrafficNode[0].InnerText
+                    };
+                    message.SuggestedTraffic.Add(googleTrafficRoute);
+                }
                 message.GenerateText();
                 return message;
             }
@@ -45,7 +60,20 @@ namespace ModiinNewsAggregator.Producers
 
         public void GenerateText()
         {
-            Text = SuggestedTraffic[0].InCurrentTraffic;
+            if (SuggestedTraffic.Count == 0)
+            {
+                Text = null;
+                return;
+            }
+
+            var text = new StringBuilder();
+            foreach (GoogleTrafficRoute googleTrafficRoute in SuggestedTraffic)
+            {
+                text.AppendFormat("{0} (Standard: {1}), ", googleTrafficRoute.InCurrentTraffic.Trim(), googleTrafficRoute.StandardTraffic);
+            }
+
+            text.Remove(text.Length-2, 2);
+            Text = text.ToString();
         }
         #region IMessage
         public bool Empty { get { return String.IsNullOrEmpty(Text); } }
@@ -55,6 +83,7 @@ namespace ModiinNewsAggregator.Producers
 
     public class GoogleTrafficRoute
     {
+        public string StandardTraffic { get; set; }
         public string InCurrentTraffic { get; set; }
     }
 }
