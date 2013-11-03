123b4025a84b41bd0dd31a163d7551323a811604
igor.zc <igor.zc@5f9a0195-93d7-7a6b-d9ba-c9014156c6cf>
2013-11-03 11:17:05 +0000

    added Up & Down for weather

--- trunk/Stable/Sources/ModiinNewsAggregator/ModiinNewsAggregator/Producers/PogodaModiinProducer.cs
+++ trunk/Stable/Sources/ModiinNewsAggregator/ModiinNewsAggregator/Producers/PogodaModiinProducer.cs
@@ -23,7 +23,7 @@ namespace ModiinNewsAggregator.Producers
             HtmlNode dataUpdated = htmlDoc.DocumentNode.SelectSingleNode(@"/html/body/div/div[4]/div[2]/div[1]/p[2]/b[1]");
             string dataUpdatedText = dataUpdated.InnerText;
 
-            return new MessageContainer(String.Format("#Modiin - now {0}\u00B0C. Source: http://pogoda.co.il/israel/modiin, updated {1}", contentStrings[0], dataUpdatedText));
+            return new PogodaMessage(contentStrings[0], dataUpdatedText);
         }
         #endregion
         string GetPogodaModiinPage()
@@ -39,6 +39,70 @@ namespace ModiinNewsAggregator.Producers
                     return s;
                 }
             }
+        }
+    }
+
+    public class PogodaMessage : IMessage
+    {
+        public bool Empty { get { return String.IsNullOrEmpty(Text); }}
+        public string Text { get; private set; }
+        public double Temperature { get; private set; }
+
+        public PogodaMessage(string temperatureAsString, string updatedDate)
+        {
+            double temperature;
+            Temperature = Double.NaN;
+            if (Double.TryParse(temperatureAsString, out temperature))
+            {
+                Temperature = temperature;
+            }
+            Text = String.Format("#Modiin - now {0}\u00B0C. Source: http://pogoda.co.il/israel/modiin, updated {1}", temperature, updatedDate);
+        }
+
+        public override string ToString()
+        {
+            return Text;
+        }
+    }
+
+    // → ← ↑ ↓
+    public class PogodaUpDownProducer : IProducer
+    {
+        private readonly IProducer actualProducer;
+        private double lastValue = double.NaN;
+        public PogodaUpDownProducer(IProducer actualProducer)
+        {
+            this.actualProducer = actualProducer;
+        }
+        public IMessage GetMessage()
+        {
+            IMessage actualMessage = actualProducer.GetMessage();
+            var pogodaMessage = actualMessage as PogodaMessage;
+            if (pogodaMessage == null)
+                return actualMessage;            
+            if (double.IsNaN(pogodaMessage.Temperature))
+                return actualMessage;
+            if (double.IsNaN(lastValue))
+            {
+                lastValue = pogodaMessage.Temperature;
+            }
+            if (Math.Abs(lastValue - pogodaMessage.Temperature) < 0.001)
+                return actualMessage;
+
+            IMessage updatedMessage = actualMessage;
+            double newValue = pogodaMessage.Temperature;
+            try
+            {             
+                if (lastValue < newValue)
+                    updatedMessage = new MessageContainer("↑ " + actualMessage.Text); 
+                if (lastValue > newValue)
+                    updatedMessage = new MessageContainer("↓ " + actualMessage.Text); 
+            }
+            finally
+            {
+                lastValue = newValue;
+            }
+            return updatedMessage;
         }
     }
 }
